034fd484914e2a8e6ca010d5da0d38080617d477
Hans Losche <hansloesche.art@gmail.com>
2021-05-30 14:54:11 -0400

    added AssetFunctions

---
+++ AssetFunctions.py
@@ -0,0 +1,25 @@
+import unreal
+
+texture_tga = '/Users/hlos/testing_unit/unreal-python/Assets/texture.TGA'
+sound_wav = '/Users/hlos/testing_unit/unreal-python/Assets/sound.WAV'
+
+def importMyAssets():
+    texture_task = buildImportTask(texture_tga, '/Game/Textures')
+    sound_task = buildImportTask(sound_wav, '/Game/Sounds')
+    executeImportTasks([texture_task, sound_task])
+
+def buildImportTask(filename, destination_path):
+    task = unreal.AssetImportTask()
+    task.set_editor_property('automated', True)
+    task.set_editor_property('destination_name', '')
+    task.set_editor_property('destination_path', destination_path)
+    task.set_editor_property('filename', filename)
+    task.set_editor_property('replace_existing', False)
+    task.set_editor_property('save', True)
+
+    return task
+
+def executeImportTasks(tasks):
+    unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks(tasks)
+
+        
